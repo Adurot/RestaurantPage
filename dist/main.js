6f6fa7f4293e2767a765150ee4095cb880ff0213
JoshTraxler <josh.traxler@gmail.com>
2022-05-25 14:06:02 -0500

    Converted HTML Code to Javascript

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("console.log(\"Testing Webpack2\")\n\n\n//# sourceURL=webpack://restaurantpage/./src/index.js?");
+eval("console.log(\"Testing Webpack2\")\n\nlet div = document.createElement(\"div\");\ndiv.id = \"title\";\ndiv.className = \"title\";\ndiv.style = \"background-color: blue;\";\ndiv.style.width = \"300px\";\ndiv.style.height = \"200px\";\ndiv.innerHTML= \"test1\";\ndocument.body.appendChild(div);  \n\nlet h1 = document.createElement(\"h1\");\n\nh1.className = \"title\";\nh1.style = \"background-color: blue;\";\nh1.style.width = \"100px\";\nh1.style.height = \"100px\";\nh1.innerHTML= \"Almond Butter Sandwhiches\";\ndocument.getElementById(\"title\").appendChild(h1);  \n\ndiv = document.createElement(\"div\");\ndiv.id = \"promo\";\ndiv.style = \"background-color: green;\";\ndiv.style.width = \"300px\";\ndiv.style.height = \"200px\";\ndiv.innerHTML= \"test1\";\ndocument.body.appendChild(div);  \n\nlet h3 = document.createElement(\"h3\");\n\nh3.className = \"title\";\nh3.style = \"background-color: blue;\";\nh3.style.width = \"400px\";\nh3.style.height = \"100px\";\nh3.innerHTML= \"Have you ever test wondered if you could ever get too many delicious almonds?  Well, now you can find out at Traxler's Almond Emporium!\";\ndocument.getElementById(\"promo\").appendChild(h3);  \n\n\n\n\n\ndiv = document.createElement(\"div\");\ndiv.id = \"div_id\";\ndiv.className = \"div_class\";\ndiv.style = \"background-color: red;\";\ndiv.style.width = \"100px\";\ndiv.style.height = \"100px\";\ndiv.innerHTML= \"Almond Butter Sandwhiches\";\ndocument.body.appendChild(div);  \n\n\n//# sourceURL=webpack://restaurantpage/./src/index.js?");
 
 /***/ })
 
--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,5 @@ module.exports = {
   },
 
 
-  mode:"development"
+  mode:'development'
 };
